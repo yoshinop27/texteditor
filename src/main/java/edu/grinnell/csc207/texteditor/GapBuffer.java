01637b2250cc91ae5c4dbb578448610ad7c2a565
reocraft <reocraft.creeper@gmail.com>
2025-09-28 02:02:52 -0500

    Part 3 completed

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -1,38 +1,117 @@
 package edu.grinnell.csc207.texteditor;
+import java.util.Arrays;
 
 /**
  * A gap buffer-based implementation of a text buffer.
  */
 public class GapBuffer {
+
+    private char[] GapBuffer;
+    private int cursor;
+    private int bufferStart;
+    private int bufferEnd;
+
+    public GapBuffer() {
+        GapBuffer = new char[10];
+        cursor = 0;
+        bufferStart = 0;
+        bufferEnd = 10;
+    }
+
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        // throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if (bufferStart == bufferEnd) {
+            GapBuffer = Arrays.copyOf(GapBuffer, GapBuffer.length * 2);
+        }
+        if (cursor <= bufferStart) {
+            for (int i = bufferStart; i > cursor; i--) {
+                GapBuffer[i] = GapBuffer[i - 1];
+            }
+            GapBuffer[cursor] = ch;
+            bufferStart++;
+            moveRight();
+        } else {
+            for (int i = bufferEnd; i < cursor; i++) {
+                GapBuffer[i - 1] = GapBuffer[i];
+            }
+            GapBuffer[cursor - 1] = ch;
+            bufferEnd--;
+        }
     }
 
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        // throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if (cursor == 0) return;
+        if (cursor <= bufferStart) {
+            for (int i = cursor; i < bufferStart; i++) {
+                GapBuffer[i - 1] = GapBuffer[i];
+            }
+            bufferStart--;
+            moveLeft();
+        } else {
+            for (int i = cursor - 1; i > bufferEnd; i--) {
+                GapBuffer[i] = GapBuffer[i - 1];
+            }
+            bufferEnd++;
+        }
     }
 
     public int getCursorPosition() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+        // throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+        if (cursor <= bufferStart) {
+            return cursor;
+        }
+        else {
+            return cursor - (bufferEnd - bufferStart);
+        }
     }
 
     public void moveLeft() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        // throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        if (cursor == bufferEnd) {
+            cursor = bufferStart;
+        }
+        else if (cursor > 0){
+            cursor--;
+        }
     }
 
     public void moveRight() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        // throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        if (cursor == bufferStart) {
+            cursor = bufferEnd;
+        }
+        else if (cursor < GapBuffer.length - 1){
+            cursor++;
+        }
     }
 
     public int getSize() {
-        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+        // throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+        return (GapBuffer.length - (bufferEnd - bufferStart));
     }
 
     public char getChar(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+        // throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+        if (i < bufferStart) {
+            return GapBuffer[i];
+        }
+        else {
+            return GapBuffer[i + (bufferEnd - bufferStart)];
+        }
     }
 
     public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        // throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        char[] front = new char[bufferStart];
+        for (int i = 0; i < bufferStart; i++) {
+            front[i] = GapBuffer[i];
+        }
+        char[] back = new char[GapBuffer.length - bufferEnd];
+        for (int i = bufferEnd; i < GapBuffer.length; i++) {
+            back[i] = GapBuffer[i];
+        }
+
+        return String.valueOf(front) + String.valueOf(back);
     }
 }
--- src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
@@ -1,5 +1,109 @@
 package edu.grinnell.csc207.texteditor;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+
 public class GapBufferTests {
     /** TODO: fill me in with unit and property tests! */
+    @Test
+    public void insert(){
+        GapBuffer s = new GapBuffer();
+        s.insert('a');
+        assertEquals(1, s.getSize());
+        assertEquals(1, s.getCursorPosition());
+        assertEquals('a', s.getChar(0));
+    }
+
+    @Test
+    public void delete(){
+        GapBuffer s = new GapBuffer();
+        s.insert('a');
+        s.delete();
+        assertEquals(0, s.getSize());
+        assertEquals(0, s.getCursorPosition());
+    }
+
+    @Test
+    public void moveLeft(){
+        GapBuffer s = new GapBuffer();
+        s.insert('a');
+        s.moveLeft();
+        assertEquals(0, s.getCursorPosition());
+    }
+
+    @Test
+    public void moveRight(){
+        GapBuffer s = new GapBuffer();
+        s.insert('a');
+        s.moveLeft();
+        s.moveRight();
+        assertEquals(1, s.getCursorPosition());
+    }
+
+    @Test
+    public void complex(){
+        GapBuffer s = new GapBuffer();
+        s.insert('a');
+        s.insert('b');
+        s.insert('c');
+        s.moveLeft();
+        s.delete();
+        s.insert('d');
+        s.moveLeft();
+        s.moveLeft();
+        s.insert('e');
+        assertEquals(4, s.getSize());
+        assertEquals(1, s.getCursorPosition());
+        assertEquals('e', s.getChar(0));
+        assertEquals('a', s.getChar(1));
+        assertEquals('d', s.getChar(2));
+        assertEquals('c', s.getChar(3));
+        assertEquals("eadc", s.toString());
+    }
+
+    @Test
+    public void moveLeftBoundaries(){
+        GapBuffer s = new GapBuffer();
+        s.moveLeft();
+        assertEquals(0, s.getCursorPosition());
+        s.insert('a');
+        s.moveLeft();
+        s.moveLeft();
+        assertEquals(0, s.getCursorPosition());
+    }
+
+    @Test
+    public void moveRightBoundaries(){
+        GapBuffer s = new GapBuffer();
+        s.moveRight();
+        assertEquals(0, s.getCursorPosition());
+        s.insert('a');
+        s.moveRight();
+        s.moveRight();
+        assertEquals(1, s.getCursorPosition());
+    }
+
+    @Test
+    public void deleteBoundaries(){
+        GapBuffer s = new GapBuffer();
+        s.delete();
+        assertEquals(0, s.getSize());
+        s.insert('a');
+        s.delete();
+        s.delete();
+        assertEquals(0, s.getSize());
+    }
+
+    @Property
+    public void insertProperty(@ForAll char ch){
+        GapBuffer s = new GapBuffer();
+        s.insert(ch);
+        assertEquals(1, s.getSize());
+        assertEquals(1, s.getCursorPosition());
+        assertEquals(ch, s.getChar(0));
+    }
 }
